05cb7dfb1baac9ed43b17f759bf56a274cc7dea0
EmnaWalha99 <ewalha12@gmail.com>
2025-11-30 13:24:48 +0100

    remove unnecessary files

--- csv_excel.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv(r"C:\Users\marie\OneDrive\Desktop\Job_Portal\job_emploisTunisie.csv")
-df.to_excel(r"C:\Users\marie\OneDrive\Desktop\Job_Portal\emploisTunisie.xlsx", index=False)
